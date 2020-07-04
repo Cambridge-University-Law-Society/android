e589e3e5b51ab35f31864e564c7938bafe411ec9
Rajan Mahoon <rm916@cam.ac.uk>
2020-07-04 14:54:02 +0100

    added a simple calendar

--- app/src/main/java/com/example/culs/CalendarFragment.java
+++ app/src/main/java/com/example/culs/CalendarFragment.java
@@ -4,16 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class CalendarFragment extends Fragment {
     @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
 
+    // Define the variable of CalendarView type
+            // and TextView type
+            CalendarView calender;
+    TextView date_view;
+
+    @Override
+    public View onCreateView
+            (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        // By ID we can use each component
+        // which id is assign in xml file
+        // use findViewById() to get the
+        // CalendarView and TextView
+        calender = (CalendarView)
+                v.findViewById(R.id.calender);
+        //date_view = (TextView)
+                //v.findViewById(R.id.date_view);
+
+        // Add Listener in calendar
+        calender
+                .setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
+
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth) {
+
+                                // Store the value of date with
+                                // format in String type Variable
+                                // Add 1 in month because month
+                                // index is start with 0
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+
+                                // set this date in TextView for Display
+                                //date_view.setText(Date);
+                            }
+                        });
+        return v;
     }
+    
+
 }
