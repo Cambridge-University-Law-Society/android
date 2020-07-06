e22e802ebff261cc8b06c3ecd3329c4c13bc7def
Rajan Mahoon <rm916@cam.ac.uk>
2020-07-06 16:19:08 +0100

    added Sign Up Page

---
+++ app/src/main/java/com/example/culs/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.culs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        
+
+    }
+}
